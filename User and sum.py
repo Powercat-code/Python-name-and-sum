2b8c1d82a409bba591306e3fa9013e0e668c44db
Powercat-code <55106022+Powercat-code@users.noreply.github.com>
2019-09-22 20:46:20 -0700

    Create User and sum.py

---
+++ User and sum.py
@@ -0,0 +1,10 @@
+#name input
+person = input("enter your name and press enter\n")
+
+#number input
+
+num1 = int(input("enter a number "))
+num2 = int(input("enter a second number "))
+sum = num1 + num2
+print("thank you {0} the sum of {1} and {2} equals {3}" .format(person, num1, num2, sum))
+
